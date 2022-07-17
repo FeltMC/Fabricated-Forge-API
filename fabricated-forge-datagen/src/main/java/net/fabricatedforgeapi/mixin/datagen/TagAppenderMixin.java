321e8d94b7969911177b3837e65938e9aba7bda9
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-07-17 18:38:09 -0400

    possibly fixed TagAppender issues

--- fabricated-forge-datagen/src/main/java/net/fabricatedforgeapi/mixin/datagen/TagAppenderMixin.java
+++ fabricated-forge-datagen/src/main/java/net/fabricatedforgeapi/mixin/datagen/TagAppenderMixin.java
@@ -3,18 +3,21 @@ package net.fabricatedforgeapi.mixin.datagen;
 import net.minecraft.core.Registry;
 import net.minecraft.data.tags.TagsProvider;
 import net.minecraft.tags.Tag;
+import net.minecraft.tags.TagKey;
 import net.minecraftforge.common.extensions.IForgeTagAppender;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(TagsProvider.TagAppender.class)
-public class TagAppenderMixin<T> implements IForgeTagAppender<T> {
+public abstract class TagAppenderMixin<T> implements IForgeTagAppender<T> {
     @Shadow @Final private Tag.Builder builder;
 
     @Shadow @Final private String source;
 
     @Shadow @Final private Registry<T> registry;
+
+    @Shadow public abstract TagsProvider.TagAppender<T> addTag(TagKey<T> tag);
 
     @Override
     public Tag.Builder getInternalBuilder() {
@@ -29,5 +32,13 @@ public class TagAppenderMixin<T> implements IForgeTagAppender<T> {
     @Override
     public Registry<T> getRegistry() {
         return this.registry;
+    }
+
+    @Override
+    public TagsProvider.TagAppender<T> addTags(TagKey<T>... values) {
+        for (TagKey<T> value : values) {
+            this.addTag(value);
+        }
+        return (TagsProvider.TagAppender<T>) (Object)this;
     }
 }
