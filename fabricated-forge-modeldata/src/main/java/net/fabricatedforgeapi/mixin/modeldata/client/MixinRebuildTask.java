1d625f322178b5d1b14a2e4c429950ddff56bbb5
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-08-22 18:14:45 -0400

    mixined fabric mixin

--- fabricated-forge-modeldata/src/main/java/net/fabricatedforgeapi/mixin/modeldata/client/MixinRebuildTask.java
+++ fabricated-forge-modeldata/src/main/java/net/fabricatedforgeapi/mixin/modeldata/client/MixinRebuildTask.java
@@ -3,6 +3,7 @@ package net.fabricatedforgeapi.mixin.modeldata.client;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.fabricatedforgeapi.modeldata.ChunkCompileTaskExtension;
+import net.fabricmc.fabric.mixin.client.indigo.renderer.MixinChunkRebuildTask;
 import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockAndTintGetter;
@@ -13,9 +14,9 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.Random;
 
-@Mixin(targets = "net.minecraft.client.renderer.chunk.ChunkRenderDispatcher$RenderChunk$RebuildTask")
+@Mixin(targets = "net.fabricmc.fabric.mixin.client.indigo.renderer.MixinChunkRebuildTask")
 public class MixinRebuildTask {
-    @Redirect(method = "compile", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/block/BlockRenderDispatcher;renderBatched(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/BlockAndTintGetter;Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;ZLjava/util/Random;)Z"))
+    @Redirect(method = "hookChunkBuildTesselate", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/block/BlockRenderDispatcher;renderBatched(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/BlockAndTintGetter;Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;ZLjava/util/Random;)Z"))
     private boolean injectCompile(BlockRenderDispatcher instance, BlockState state, BlockPos pos, BlockAndTintGetter level, PoseStack poseStack, VertexConsumer consumer, boolean checkSides, Random random){
         return instance.renderBatched(state, pos, level, poseStack, consumer, checkSides, random, ((ChunkCompileTaskExtension)this).getModelData(pos));
     }
