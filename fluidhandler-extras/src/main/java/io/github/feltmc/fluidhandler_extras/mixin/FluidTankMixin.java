eaa6ad3916a111086d0a4e1c188591d571a2a0ad
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-10-18 19:56:53 -0400

    tried fixing defaul method clash in fluidhandler-extras

--- fluidhandler-extras/src/main/java/io/github/feltmc/fluidhandler_extras/mixin/FluidTankMixin.java
+++ fluidhandler-extras/src/main/java/io/github/feltmc/fluidhandler_extras/mixin/FluidTankMixin.java
@@ -1,6 +1,5 @@
 package io.github.feltmc.fluidhandler_extras.mixin;
 
-import io.github.feltmc.fluidhandler_extras.IFluidTankExtension;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.capability.IFluidHandler;
 import net.minecraftforge.fluids.capability.templates.FluidTank;
@@ -8,8 +7,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(value = FluidTank.class, remap = false)
-public abstract class FluidTankMixin implements IFluidTankExtension {
-    @Override
+public abstract class FluidTankMixin {
     public long getFluidAmountInDroplets(){
         return getFluidAmount();
     }
@@ -20,7 +18,6 @@ public abstract class FluidTankMixin implements IFluidTankExtension {
     /**
      * @return Capacity of this fluid tank.
      */
-    @Override
     public long getCapacityInDroplets(){
         return getCapacity();
     }
@@ -31,12 +28,9 @@ public abstract class FluidTankMixin implements IFluidTankExtension {
     abstract int fill(FluidStack stack, IFluidHandler.FluidAction action);
     @Shadow
     abstract FluidStack drain(int i, IFluidHandler.FluidAction fluidAction);
-    @Override
     public long fillDroplets(FluidStack stack, IFluidHandler.FluidAction action){
         return fill(stack, action);
     }
-
-    @Override
     public FluidStack drain(long i, IFluidHandler.FluidAction fluidAction){
         return drain((int)i, fluidAction);
     }
--- fluidhandler-extras/src/main/java/io/github/feltmc/fluidhandler_extras/mixin/IFluidTankMixin.java
+++ fluidhandler-extras/src/main/java/io/github/feltmc/fluidhandler_extras/mixin/IFluidTankMixin.java
@@ -1,6 +1,5 @@
 package io.github.feltmc.fluidhandler_extras.mixin;
 
-import io.github.feltmc.fluidhandler_extras.IFluidTankExtension;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.IFluidTank;
 import net.minecraftforge.fluids.capability.IFluidHandler;
@@ -8,8 +7,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(value = IFluidTank.class, remap = false)
-public interface IFluidTankMixin extends IFluidTankExtension {
-    @Override
+public interface IFluidTankMixin {
     default long getFluidAmountInDroplets(){
         return getFluidAmount();
     }
@@ -20,7 +18,6 @@ public interface IFluidTankMixin extends IFluidTankExtension {
     /**
      * @return Capacity of this fluid tank.
      */
-    @Override
     default long getCapacityInDroplets(){
         return getCapacity();
     }
@@ -31,12 +28,10 @@ public interface IFluidTankMixin extends IFluidTankExtension {
     int fill(FluidStack stack, IFluidHandler.FluidAction action);
     @Shadow
     FluidStack drain(int i, IFluidHandler.FluidAction fluidAction);
-    @Override
     default long fillDroplets(FluidStack stack, IFluidHandler.FluidAction action){
         return fill(stack, action);
     }
 
-    @Override
     default FluidStack drain(long i, IFluidHandler.FluidAction fluidAction){
         return drain((int)i, fluidAction);
     }
