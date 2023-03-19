8e8207b18600842871dc0975a09870ec3264b7ce
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-03-18 22:30:22 -0400

    added methods in ModelBlockRenderer and BlockRenderDispatcher are now reimplementations of vanilla methods

--- fabricated-forge-modeldata/src/main/java/net/fabricatedforgeapi/mixin/modeldata/client/MixinBlockRenderDispatcher.java
+++ fabricated-forge-modeldata/src/main/java/net/fabricatedforgeapi/mixin/modeldata/client/MixinBlockRenderDispatcher.java
@@ -3,15 +3,27 @@ package net.fabricatedforgeapi.mixin.modeldata.client;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.fabricatedforgeapi.modeldata.BlockRenderDispatcherExtension;
+import net.minecraft.CrashReport;
+import net.minecraft.CrashReportCategory;
+import net.minecraft.ReportedException;
+import net.minecraft.client.color.block.BlockColors;
+import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.block.BlockModelShaper;
 import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.client.renderer.block.ModelBlockRenderer;
+import net.minecraft.client.renderer.block.model.ItemTransforms;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.client.model.data.EmptyModelData;
 import net.minecraftforge.client.model.data.IModelData;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -22,50 +34,52 @@ import java.util.Random;
 
 @Mixin(BlockRenderDispatcher.class)
 public abstract class MixinBlockRenderDispatcher implements BlockRenderDispatcherExtension {
-    @Shadow public abstract boolean renderBatched(BlockState state, BlockPos pos, BlockAndTintGetter level, PoseStack poseStack, VertexConsumer consumer, boolean checkSides, Random random);
+    @Shadow @Final private ModelBlockRenderer modelRenderer;
 
-    @Shadow public abstract void renderSingleBlock(BlockState state, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, int packedOverlay);
+    @Shadow public abstract BakedModel getBlockModel(BlockState state);
 
-    @Shadow public abstract void renderBreakingTexture(BlockState state, BlockPos pos, BlockAndTintGetter level, PoseStack poseStack, VertexConsumer consumer);
-
-    @Unique private IModelData modelData = EmptyModelData.INSTANCE;
-    @Unique private IModelData modelData1 = EmptyModelData.INSTANCE;
-    @Unique private IModelData modelData2 = EmptyModelData.INSTANCE;
-
-    @Redirect(method = "renderBatched", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/block/ModelBlockRenderer;tesselateBlock(Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/client/resources/model/BakedModel;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;ZLjava/util/Random;JI)Z"))
-    private boolean redirectRenderBatched(ModelBlockRenderer instance, BlockAndTintGetter level, BakedModel model, BlockState state, BlockPos pos, PoseStack poseStack, VertexConsumer consumer, boolean checkSides, Random random, long seed, int packedOverlay){
-        return instance.tesselateBlock(level, model, state, pos, poseStack, consumer, checkSides, random, seed, packedOverlay, modelData);
-    }
-
-    @Redirect(method = "renderSingleBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/block/ModelBlockRenderer;renderModel(Lcom/mojang/blaze3d/vertex/PoseStack$Pose;Lcom/mojang/blaze3d/vertex/VertexConsumer;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/client/resources/model/BakedModel;FFFII)V"))
-    private void redirectRenderSingleBlock(ModelBlockRenderer instance, PoseStack.Pose pose, VertexConsumer consumer, BlockState state, BakedModel model, float red, float green, float blue, int packedLight, int packedOverlay){
-        instance.renderModel(pose, consumer, state, model, red, green, blue, packedLight, packedOverlay, modelData1);
-    }
-
-    @Redirect(method = "renderBreakingTexture", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/block/ModelBlockRenderer;tesselateBlock(Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/client/resources/model/BakedModel;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;ZLjava/util/Random;JI)Z"))
-    private boolean redirectBreaking(ModelBlockRenderer instance, BlockAndTintGetter level, BakedModel model, BlockState state, BlockPos pos, PoseStack poseStack, VertexConsumer consumer, boolean checkSides, Random random, long seed, int packedOverlay){
-        return instance.tesselateBlock(level, model, state, pos, poseStack, consumer, checkSides, random, seed, packedOverlay, modelData2);
-    }
+    @Shadow @Final private BlockColors blockColors;
+    @Shadow @Final private BlockEntityWithoutLevelRenderer blockEntityRenderer;
+    @Shadow @Final private BlockModelShaper blockModelShaper;
+    @Shadow @Final private Random random;
 
     @Override
-    public boolean renderBatched(BlockState arg, BlockPos arg2, BlockAndTintGetter arg3, PoseStack arg4, VertexConsumer arg5, boolean bl, Random random, IModelData modelData) {
-        this.modelData = modelData;
-        boolean render = renderBatched(arg, arg2, arg3, arg4, arg5, bl, random);
-        this.modelData = EmptyModelData.INSTANCE;
-        return render;
+    public boolean renderBatched(BlockState state, BlockPos pos, BlockAndTintGetter level, PoseStack poseStack, VertexConsumer consumer, boolean checkSides, Random random, IModelData modelData) {
+        try {
+            RenderShape renderShape = state.getRenderShape();
+            return renderShape == RenderShape.MODEL && this.modelRenderer.tesselateBlock(level, this.getBlockModel(state), state, pos, poseStack, consumer, checkSides, random, state.getSeed(pos), OverlayTexture.NO_OVERLAY, modelData);
+        } catch (Throwable var11) {
+            CrashReport crashReport = CrashReport.forThrowable(var11, "Tesselating block in world");
+            CrashReportCategory crashReportCategory = crashReport.addCategory("Block being tesselated");
+            CrashReportCategory.populateBlockDetails(crashReportCategory, level, pos, state);
+            throw new ReportedException(crashReport);
+        }
     }
 
-    @Override
-    public void renderSingleBlock(BlockState arg, PoseStack arg2, MultiBufferSource arg3, int j, int k, IModelData modelData) {
-        this.modelData1 = modelData;
-        this.renderSingleBlock(arg, arg2, arg3, j, k);
-        this.modelData1 = EmptyModelData.INSTANCE;
+    public void renderSingleBlock(BlockState state, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, int packedOverlay, IModelData data) {
+        RenderShape renderShape = state.getRenderShape();
+        if (renderShape != RenderShape.INVISIBLE) {
+            switch (renderShape) {
+                case MODEL:
+                    BakedModel bakedModel = this.getBlockModel(state);
+                    int i = this.blockColors.getColor(state, (BlockAndTintGetter)null, (BlockPos)null, 0);
+                    float f = (float)(i >> 16 & 255) / 255.0F;
+                    float g = (float)(i >> 8 & 255) / 255.0F;
+                    float h = (float)(i & 255) / 255.0F;
+                    this.modelRenderer.renderModel(poseStack.last(), bufferSource.getBuffer(ItemBlockRenderTypes.getRenderType(state, false)), state, bakedModel, f, g, h, packedLight, packedOverlay, data);
+                    break;
+                case ENTITYBLOCK_ANIMATED:
+                    this.blockEntityRenderer.renderByItem(new ItemStack(state.getBlock()), ItemTransforms.TransformType.NONE, poseStack, bufferSource, packedLight, packedOverlay);
+            }
+
+        }
     }
 
-    @Override
-    public void renderBreakingTexture(BlockState arg, BlockPos arg2, BlockAndTintGetter arg3, PoseStack arg4, VertexConsumer arg5, IModelData modelData) {
-        this.modelData2 = modelData;
-        this.renderBreakingTexture(arg, arg2, arg3, arg4, arg5);
-        this.modelData2 = EmptyModelData.INSTANCE;
+    public void renderBreakingTexture(BlockState state, BlockPos pos, BlockAndTintGetter level, PoseStack poseStack, VertexConsumer consumer, IModelData modelData) {
+        if (state.getRenderShape() == RenderShape.MODEL) {
+            BakedModel bakedModel = this.blockModelShaper.getBlockModel(state);
+            long l = state.getSeed(pos);
+            this.modelRenderer.tesselateBlock(level, bakedModel, state, pos, poseStack, consumer, true, this.random, l, OverlayTexture.NO_OVERLAY, modelData);
+        }
     }
 }
--- fabricated-forge-modeldata/src/main/java/net/fabricatedforgeapi/mixin/modeldata/client/MixinModelBlockRenderer.java
+++ fabricated-forge-modeldata/src/main/java/net/fabricatedforgeapi/mixin/modeldata/client/MixinModelBlockRenderer.java
@@ -3,90 +3,127 @@ package net.fabricatedforgeapi.mixin.modeldata.client;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.fabricatedforgeapi.modeldata.ModelBlockRendererExtension;
+import net.minecraft.CrashReport;
+import net.minecraft.CrashReportCategory;
+import net.minecraft.ReportedException;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.block.ModelBlockRenderer;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.client.model.data.EmptyModelData;
 import net.minecraftforge.client.model.data.IModelData;
-import org.spongepowered.asm.mixin.Debug;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import javax.annotation.Nullable;
+import java.util.BitSet;
 import java.util.List;
 import java.util.Random;
+
+import static net.minecraft.client.renderer.LevelRenderer.DIRECTIONS;
 
 @Debug(export = true)
 @Mixin(value = ModelBlockRenderer.class, priority = 1100)
 public abstract class MixinModelBlockRenderer implements ModelBlockRendererExtension {
-    @Shadow public abstract boolean tesselateBlock(BlockAndTintGetter level, BakedModel model, BlockState state, BlockPos pos, PoseStack poseStack, VertexConsumer consumer, boolean checkSides, Random random, long seed, int packedOverlay);
+    @Shadow protected abstract void renderModelFaceFlat(BlockAndTintGetter level, BlockState state, BlockPos pos, int packedLight, int packedOverlay, boolean repackLight, PoseStack poseStack, VertexConsumer consumer, List<BakedQuad> quads, BitSet shapeFlags);
 
-    @Shadow public abstract boolean tesselateWithAO(BlockAndTintGetter level, BakedModel model, BlockState state, BlockPos pos, PoseStack poseStack, VertexConsumer consumer, boolean checkSides, Random random, long seed, int packedOverlay);
+    @Shadow @Final private static Direction[] DIRECTIONS;
 
-    @Shadow public abstract boolean tesselateWithoutAO(BlockAndTintGetter level, BakedModel model, BlockState state, BlockPos pos, PoseStack poseStack, VertexConsumer consumer, boolean checkSides, Random random, long seed, int packedOverlay);
+    @Shadow protected abstract void renderModelFaceAO(BlockAndTintGetter level, BlockState state, BlockPos pos, PoseStack poseStack, VertexConsumer consumer, List<BakedQuad> quads, float[] shape, BitSet shapeFlags, ModelBlockRenderer.AmbientOcclusionFace aoFace, int packedOverlay);
 
-    @Shadow public abstract void renderModel(PoseStack.Pose pose, VertexConsumer consumer, @org.jetbrains.annotations.Nullable BlockState state, BakedModel model, float red, float green, float blue, int packedLight, int packedOverlay);
-
-    @Unique private IModelData modelData = EmptyModelData.INSTANCE;
-    @Unique private IModelData modelData1 = EmptyModelData.INSTANCE;
-    @Unique private IModelData modelData2 = EmptyModelData.INSTANCE;
-    @Unique private IModelData modelData3 = EmptyModelData.INSTANCE;
-    @Redirect(method = "tesselateBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/block/ModelBlockRenderer;tesselateWithoutAO(Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/client/resources/model/BakedModel;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;ZLjava/util/Random;JI)Z"))
-    private boolean redirectTesselateWithoutAO(ModelBlockRenderer instance, BlockAndTintGetter level, BakedModel model, BlockState state, BlockPos pos, PoseStack poseStack, VertexConsumer consumer, boolean checkSides, Random random, long seed, int packedOverlay){
-        return tesselateWithoutAO(level, model, state, pos, poseStack, consumer, checkSides, random, seed, packedOverlay, modelData);
+    @Shadow
+    protected static void renderQuadList(PoseStack.Pose pose, VertexConsumer consumer, float red, float green, float blue, List<BakedQuad> quads, int packedLight, int packedOverlay) {
     }
 
-    @Redirect(method = "tesselateBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/block/ModelBlockRenderer;tesselateWithAO(Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/client/resources/model/BakedModel;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;ZLjava/util/Random;JI)Z"))
-    private boolean redirectTesselateWithAO(ModelBlockRenderer instance, BlockAndTintGetter level, BakedModel model, BlockState state, BlockPos pos, PoseStack poseStack, VertexConsumer consumer, boolean checkSides, Random random, long seed, int packedOverlay){
-        return tesselateWithAO(level, model, state, pos, poseStack, consumer, checkSides, random, seed, packedOverlay, modelData);
-    }
-    @Redirect(method = {"tesselateWithAO"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/resources/model/BakedModel;getQuads(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/Direction;Ljava/util/Random;)Ljava/util/List;"))
-    private List<BakedQuad> redirectGetQuadsWithAO(BakedModel instance, BlockState state, Direction direction, Random random){
-        return instance.getQuads(state, direction, random, this.modelData1);
-    }
+    public boolean tesselateBlock(BlockAndTintGetter level, BakedModel model, BlockState state, BlockPos pos, PoseStack poseStack, VertexConsumer consumer, boolean checkSides, Random random, long seed, int packedOverlay, IModelData data) {
+        boolean bl = Minecraft.useAmbientOcclusion() && state.getLightEmission() == 0 && model.useAmbientOcclusion();
+        Vec3 vec3 = state.getOffset(level, pos);
+        poseStack.translate(vec3.x, vec3.y, vec3.z);
 
-    @Redirect(method = {"tesselateWithoutAO"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/resources/model/BakedModel;getQuads(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/Direction;Ljava/util/Random;)Ljava/util/List;"))
-    private List<BakedQuad> redirectGetQuadsWithoutAO(BakedModel instance, BlockState state, Direction direction, Random random){
-        return instance.getQuads(state, direction, random, this.modelData2);
-    }
-
-    @Redirect(method = {"renderModel"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/resources/model/BakedModel;getQuads(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/Direction;Ljava/util/Random;)Ljava/util/List;"))
-    private List<BakedQuad> redirectGetQuadsModel(BakedModel instance, BlockState state, Direction direction, Random random){
-        return instance.getQuads(state, direction, random, this.modelData3);
-    }
-    public boolean tesselateBlock(BlockAndTintGetter level, BakedModel model, BlockState state, BlockPos pos, PoseStack poseStack, VertexConsumer consumer, boolean checkSides, Random random, long seed, int packedOverlay, IModelData modelData){
-        modelData = model.getModelData(level, pos, state, modelData);
-        this.modelData = modelData;
-        boolean tesselate = tesselateBlock(level, model, state, pos, poseStack, consumer, checkSides, random, seed, packedOverlay);
-        this.modelData = EmptyModelData.INSTANCE;
-        return tesselate;
+        try {
+            return bl ? this.tesselateWithAO(level, model, state, pos, poseStack, consumer, checkSides, random, seed, packedOverlay, data) : this.tesselateWithoutAO(level, model, state, pos, poseStack, consumer, checkSides, random, seed, packedOverlay, data);
+        } catch (Throwable var17) {
+            CrashReport crashReport = CrashReport.forThrowable(var17, "Tesselating block model");
+            CrashReportCategory crashReportCategory = crashReport.addCategory("Block model being tesselated");
+            CrashReportCategory.populateBlockDetails(crashReportCategory, level, pos, state);
+            crashReportCategory.setDetail("Using AO", bl);
+            throw new ReportedException(crashReport);
+        }
     }
 
     public boolean tesselateWithAO(BlockAndTintGetter level, BakedModel model, BlockState state, BlockPos pos, PoseStack poseStack, VertexConsumer consumer, boolean checkSides, Random random, long seed, int packedOverlay, IModelData data) {
-        this.modelData1 = data;
-        boolean tesselate = this.tesselateWithAO(level, model, state, pos, poseStack, consumer, checkSides, random, seed, packedOverlay);
-        this.modelData1 = EmptyModelData.INSTANCE;
-        return tesselate;
+        boolean bl = false;
+        float[] fs = new float[DIRECTIONS.length * 2];
+        BitSet bitSet = new BitSet(3);
+        ModelBlockRenderer.AmbientOcclusionFace ambientOcclusionFace = ((ModelBlockRenderer)(Object)this).new AmbientOcclusionFace();
+        BlockPos.MutableBlockPos mutableBlockPos = pos.mutable();
+
+        for (Direction dir : DIRECTIONS) {
+            random.setSeed(seed);
+            List<BakedQuad> list = model.getQuads(state, dir, random, data);
+            if (!list.isEmpty()) {
+                mutableBlockPos.setWithOffset(pos, dir);
+                if (!checkSides || Block.shouldRenderFace(state, level, pos, dir, mutableBlockPos)) {
+                    this.renderModelFaceAO(level, state, pos, poseStack, consumer, list, fs, bitSet, ambientOcclusionFace, packedOverlay);
+                    bl = true;
+                }
+            }
+        }
+
+        random.setSeed(seed);
+        List<BakedQuad> list2 = model.getQuads(state, (Direction)null, random, data);
+        if (!list2.isEmpty()) {
+            this.renderModelFaceAO(level, state, pos, poseStack, consumer, list2, fs, bitSet, ambientOcclusionFace, packedOverlay);
+            bl = true;
+        }
+
+        return bl;
     }
 
     public boolean tesselateWithoutAO(BlockAndTintGetter level, BakedModel model, BlockState state, BlockPos pos, PoseStack poseStack, VertexConsumer consumer, boolean checkSides, Random random, long seed, int packedOverlay, IModelData data) {
-        this.modelData2 = data;
-        boolean tesselate = this.tesselateWithoutAO(level, model, state, pos, poseStack, consumer, checkSides, random, seed, packedOverlay);
-        this.modelData2 = EmptyModelData.INSTANCE;
-        return tesselate;
+        boolean bl = false;
+        BitSet bitSet = new BitSet(3);
+        BlockPos.MutableBlockPos mutableBlockPos = pos.mutable();
+
+        for (Direction dir : DIRECTIONS){
+            random.setSeed(seed);
+            List<BakedQuad> list = model.getQuads(state, dir, random, data);
+            if (!list.isEmpty()) {
+                mutableBlockPos.setWithOffset(pos, dir);
+                if (!checkSides || Block.shouldRenderFace(state, level, pos, dir, mutableBlockPos)) {
+                    int i = LevelRenderer.getLightColor(level, state, mutableBlockPos);
+                    this.renderModelFaceFlat(level, state, pos, i, packedOverlay, false, poseStack, consumer, list, bitSet);
+                    bl = true;
+                }
+            }
+        }
+
+        random.setSeed(seed);
+        List<BakedQuad> list2 = model.getQuads(state, (Direction)null, random, data);
+        if (!list2.isEmpty()) {
+            this.renderModelFaceFlat(level, state, pos, -1, packedOverlay, true, poseStack, consumer, list2, bitSet);
+            bl = true;
+        }
+
+        return bl;
     }
 
-    public void renderModel(PoseStack.Pose pose, VertexConsumer consumer, @Nullable BlockState state, BakedModel model, float red, float green, float blue, int packedLight, int packedOverlay, IModelData data) {
-        this.modelData3 = data;
-        this.renderModel(pose, consumer, state, model, red, green, blue, packedLight, packedOverlay);
-        this.modelData3 = EmptyModelData.INSTANCE;
+    public void renderModel(PoseStack.Pose pose, VertexConsumer consumer, @org.jetbrains.annotations.Nullable BlockState state, BakedModel model, float red, float green, float blue, int packedLight, int packedOverlay, IModelData data) {
+        Random random = new Random();
+        for (Direction dir : DIRECTIONS) {
+            random.setSeed(42L);
+            renderQuadList(pose, consumer, red, green, blue, model.getQuads(state, dir, random, data), packedLight, packedOverlay);
+        }
+        random.setSeed(42L);
+        renderQuadList(pose, consumer, red, green, blue, model.getQuads(state, (Direction)null, random, data), packedLight, packedOverlay);
     }
 }
